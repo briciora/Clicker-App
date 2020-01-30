b19b52368e136655185fc889610198f8e74354a2
Bri Ciora <briciora@Bris-MacBook-Pro.local>
2020-01-30 17:19:40 -0600

    added to DB

--- app/src/main/java/com/example/clicker/ui/login/ClassListActivity.java
+++ app/src/main/java/com/example/clicker/ui/login/ClassListActivity.java
@@ -1,9 +1,14 @@
 package com.example.clicker.ui.login;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import com.example.clicker.R;
-
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ClassListActivity extends AppCompatActivity {
@@ -12,5 +17,53 @@ public class ClassListActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_list);
+        addToDB();
+    }
+
+    void addToDB()
+    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Courses");
+        myRef.setValue("Databases");
+    }
+
+    void addClass(View view)
+    {
+        // get prompts.xml view
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompts, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(promptsView);
+
+        final EditText courseName = promptsView.findViewById(R.id.etName);
+        final EditText courseID = promptsView.findViewById(R.id.etID);
+
+        // set dialog message
+        alertDialogBuilder.setCancelable(false).setPositiveButton("SAVE",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        final String name = courseName.getText().toString();
+                        final String id_ = courseID.getText().toString();
+
+                        //ADD BACKEND TO STORE IN DB
+                    }
+                })
+                .setNegativeButton("C" +
+                                "ancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
 }
